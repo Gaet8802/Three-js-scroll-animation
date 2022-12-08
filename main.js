0601a86c3475d93b1d15d9bc645d312bba0d880f
Gaet8802 <g.micolatorre@gmail.com>
2022-12-08 14:59:50 +0100

    feat: adding a 3D rotating donut

---
+++ main.js
@@ -0,0 +1,38 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+camera.position.setZ(30);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
